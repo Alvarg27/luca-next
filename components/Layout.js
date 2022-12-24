dcf08ae5531cdd8414fc1867c9853e159f77e31c
Alvarg27 <alvargbesne@gmail.com>
2022-12-23 18:35:51 -0600

    update

--- components/Layout.js
+++ components/Layout.js
@@ -3,7 +3,7 @@ import Header from "./Header";
 
 const Layout = ({ children }) => {
   return (
-    <div className="bg-gray-100 min-h-screen">
+    <div className="bg-gray-100 h-screen max-h-screen overflow-hidden">
       <Header />
       {children}
     </div>

fd5e28b82312fa5afd314e6e11fad7abfce84c5c
Alvarg27 <alvargbesne@gmail.com>
2022-12-23 18:39:46 -0600

    update

--- components/Inicio.js
+++ components/Inicio.js
@@ -9,9 +9,9 @@ const Inicio = () => {
     <div className="flex justify-center relative mt-[200px]">
       <div className="h-[100px] w-[100px] bg-teal-400 rounded-full "></div>
       <div className="absolute w-full h-full animate-spin-slow">
-        <SmallCircle className="w-[100%] lg:w-[60%] left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2" />
-        <MediumCircle className="w-[160%] lg:w-[100%] left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2" />
-        <LargeCircle className="w-[170%]  lg:w-[105%] left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2" />
+        <SmallCircle className="w-[130%] lg:w-[60%] left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2" />
+        <MediumCircle className="w-[185%] lg:w-[100%] left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2" />
+        <LargeCircle className="w-[200%]  lg:w-[105%] left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2" />
       </div>
     </div>
   );
--- components/Layout.js
+++ components/Layout.js
@@ -3,7 +3,7 @@ import Header from "./Header";
 
 const Layout = ({ children }) => {
   return (
-    <div className="bg-gray-100 h-screen max-h-screen overflow-hidden">
+    <div className="bg-gray-100 h-screen  overflow-hidden fixed">
       <Header />
       {children}
     </div>

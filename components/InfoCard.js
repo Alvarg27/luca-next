c43332808f0103b9cc55d53c2c303b3d056676a3
Alvarg27 <alvargbesne@gmail.com>
2022-12-23 21:38:07 -0600

    Update InfoCard.js

--- components/InfoCard.js
+++ components/InfoCard.js
@@ -1,5 +1,10 @@
 import React from "react";
-
+import {
+  RiAwardLine,
+  RiLightbulbLine,
+  RiTimeLine,
+  RiTimer2Line,
+} from "react-icons/ri";
 const InfoCard = () => {
   return (
     <div className="w-[90%] text-lg">
@@ -12,18 +17,26 @@ const InfoCard = () => {
       </div>
       <div>
         <div className="flex">
-          <div className="mr-1 flex-1 bg-[#DDD8CD] rounded-2xl p-4 h-[120px]  flex flex-col justify-end">
-            <p className="font-alga">La mejor calidad en nuestros productos.</p>
+          <div className="mr-1 flex-1 bg-amber-300 rounded-2xl p-4  flex flex-col justify-between">
+            <RiAwardLine className="text-2xl" />
+            <p className="font-alga" mt-4>
+              La mejor calidad en nuestros productos.
+            </p>
           </div>
           <div
-            className="ml-1 flex-1 bg-[#DDD8CD]
-           rounded-2xl p-4 h-[120px]  flex flex-col justify-end"
+            className="ml-1 flex-1 bg-indigo-300
+           rounded-2xl p-4 flex flex-col justify-between"
           >
-            <p className="font-alga">Desarrollando ideas innovadoras.</p>
+            <RiLightbulbLine className="text-2xl" />
+            <p className="font-alga mt-4">Desarrollando ideas innovadoras.</p>
           </div>
         </div>
-        <div className="mt-2 flex-1 bg-[#DDD8CD] rounded-2xl p-4 h-[120px] flex flex-col justify-end">
-          <p className="font-alga">La mejor calidad en nuestros productos.</p>
+        <div className="mt-2 flex-1 bg-blue-300 rounded-2xl p-4 flex flex-col justify-between">
+          <RiTimer2Line className="text-2xl" />
+          <p className="font-alga mt-4">
+            Optimizando tiempo siendo así mas eficientes en el tiempo de
+            realización y entrega.
+          </p>
         </div>
       </div>
     </div>

43b0b3f960f926a99b4567a00ec6af6a97283d0c
Alvarg27 <alvargbesne@gmail.com>
2022-12-23 23:36:11 -0600

    update

--- components/Circles.js
+++ components/Circles.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import LargeCircle from "./LargeCircle";
 import MediumCircle from "./MediumCircle";
 import SmallCircle from "./SmallCircle";
 const Circles = () => {
+  const [transition, setTransition] = useState();
+  useEffect(() => {
+    setTimeout(() => {
+      setTransition(true);
+    }, 1700);
+  }, []);
   return (
     <div className="absolute w-full h-full animate-spin-slow top-0 z-[1] transition duration-300">
       <div className="absolute h-[100px] w-[100px] bg-teal-500 rounded-full left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 transition duration-300 z-[2]"></div>
-      <div className="absolute h-[120px] w-[120px] bg-teal-500 rounded-full left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 transition duration-300 bg-opacity-40 blur-md animate-pulse"></div>
+      <div
+        className={`absolute h-[120px] w-[120px] bg-teal-500 rounded-full left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 bg-opacity-40 blur-md animate-pulse transition duration-300 ${
+          transition ? "opacity-0" : "opacity-100"
+        }`}
+      ></div>
       <SmallCircle className="w-[130%] lg:w-[60%] left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 transition duration-300" />
       <MediumCircle className="w-[200%] lg:w-[100%] left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 transition duration-300" />
       <LargeCircle className="w-[210%]  lg:w-[105%] left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 transition duration-300" />

e46e0965e78a6df56c4e725baada9fd6f1f3ea0e
sjimenezbauer <sjimenezbauer@gmail.com>
2024-03-12 12:24:54 -0600

    Add google tag manager

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,44 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="es">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="es">
+        <Head>
+          {/* Google Tag Manager */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-5BDSG7S7');
+              `,
+            }}
+          />
+          {/* End Google Tag Manager */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-5BDSG7S7"
+              height="0"
+              width="0"
+              style="display:none;visibility:hidden"
+            ></iframe>
+          </noscript>
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;

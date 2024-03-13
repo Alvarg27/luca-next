9d516a1ab646d08afdf0dd72d3dd82862e8adba8
sjimenezbauer <sjimenezbauer@gmail.com>
2024-03-13 17:18:41 -0600

    Final fix gtag

--- components/layouts/Layout.js
+++ components/layouts/Layout.js
@@ -2,7 +2,7 @@ import useNavigation from '@/hooks/useNavigation';
 import usePageOffset from '@/hooks/usePageOffset';
 import useWindowDimensions from '@/hooks/useWindowDimensions';
 import Head from 'next/head';
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import MenuIcon from '../commons/MenuIcon';
 import Footer from '../Footer/Footer';
 import DropdownMenu from '../Header/DropwdownMenu';
@@ -17,6 +17,15 @@ const Layout = ({ children }) => {
   return (
     <div className="w-full max-w-full overflow-hidden ">
       <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-5BDSG7S7');`,
+          }}
+        />
         <title>Luca experiences</title>
         <meta
           name="description"
@@ -27,7 +36,6 @@ const Layout = ({ children }) => {
           content="width=device-width, initial-scale=1"
         />
         <link rel="icon" href="/luca-icon.png" />
-        
       </Head>
       {width <= 1024 && (
         <MenuIcon className="!fixed right-[5%] mt-[20px] z-[100]" />
@@ -44,10 +52,17 @@ const Layout = ({ children }) => {
       />
 
       <Header />
-      <div className=" min-h-screen flex flex-col justify-between">
-        {children}
-        <Footer />
-      </div>
+      <body>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-5BDSG7S7"
+          height="0"
+          width="0"
+        ></iframe>
+        <div className=" min-h-screen flex flex-col justify-between">
+          {children}
+          <Footer />
+        </div>
+      </body>
       <div>
         <WhatsAppButton />
       </div>
--- pages/_document.js
+++ pages/_document.js
@@ -11,22 +11,9 @@ class MyDocument extends Document {
     return (
       <Html lang="es">
         <Head>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-5BDSG7S7');`,
-            }}
-          />
+
         </Head>
         <body>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-5BDSG7S7"
-            height="0"
-            width="0"
-          ></iframe>
           <Main />
           <NextScript />
         </body>

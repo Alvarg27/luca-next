ce12f8a2d615c56b6aa178be85fbccad3d9f1976
sjimenezbauer <sjimenezbauer@gmail.com>
2024-03-14 11:10:47 -0600

    Final Fix GTAG

--- pages/_app.js
+++ pages/_app.js
@@ -28,6 +28,10 @@ export default function App({ Component, pageProps }) {
               />
               <Script
                 strategy="afterInteractive"
+                src="https://www.googletagmanager.com/ns.html?id=GTM-5BDSG7S7"
+              />
+              <Script
+                strategy="afterInteractive"
                 id="google-analytics"
               >
                 {`
@@ -37,6 +41,17 @@ export default function App({ Component, pageProps }) {
                   gtag('config', 'GTM-5BDSG7S7');
                   `}
               </Script>
+              <Script
+                id="gtm-script"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                  __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5BDSG7S7');`,
+                }}
+              />
               <Component {...pageProps} />
             </Layout>
           </PageOffsetProvider>
